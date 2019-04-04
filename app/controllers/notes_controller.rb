fd26abda47018d62e61e4c674c65440c31cf77f9
Luis Ventura <llovoven@uncc.edu>
2019-04-04 18:11:55 +0000

    Added save functionality to git

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,8 +1,21 @@
 class NotesController < ApplicationController
+    def show
+        @note = Note.find(params[:id])
+    end
+    
     def new
+        @note = Note.new 
     end
     
     def create
-        render plain: params[:article].inspect
+        @note = Note.new(note_params)
+        
+        @note.save
+        redirect_to @note
     end
 end
+
+private
+    def note_params
+        params.require(:note).permit(:title, :text)
+    end
